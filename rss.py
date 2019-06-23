aa81fe7b49f64ab4b7cd97b4e6e2b40605ee0ca2
Elia Fry <Elia@fryzen.net>
2019-06-23 02:59:48 +0200

    added dependencies / main bot loop / rss parser

---
+++ rss.py
@@ -0,0 +1,31 @@
+import feedparser
+import re
+
+feed = feedparser.parse("https://moodle.bbbaden.ch/rss/file.php/1/***REMOVED***/blog/user/9999/rss.xml")
+
+
+def sanitiseHTML(html):
+    for line in html:
+        cleanr = re.compile('(<\/?(li|ul|p|br) ?\/?>)')
+        html = re.sub(cleanr, '', html)
+
+    return html
+
+
+def getAllEntries():
+    entries = {}
+    for entry in feed.entries:
+        entries[entry.published, "title"] = [feed.entries[0].title]
+        entries[entry.published, "summary"] = [feed.entries[0].summary]
+
+    print(entries["Mon, 17 Jun 2019 15:44:08 GMT", "title"])
+
+    return entries
+
+
+def getLastEntry():
+    entry = {}
+    entry["published"] = feed.entries[0].published
+    entry["title"] = feed.entries[0].title
+    entry["summary"] = sanitiseHTML(feed.entries[0].summary)
+    return entry
